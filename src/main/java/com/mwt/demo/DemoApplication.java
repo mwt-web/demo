41b7239b65736c426bf281d22d5343a765b778bd
muwentao <P@ssw0rd>
2022-04-22 17:21:43 +0800

    mybatis-plus报错已解决，原因是pom依赖导致

--- src/main/java/com/mwt/demo/DemoApplication.java
+++ src/main/java/com/mwt/demo/DemoApplication.java
@@ -3,8 +3,6 @@ package com.mwt.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * @author muwentao
